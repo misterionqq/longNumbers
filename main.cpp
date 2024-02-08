142ac2ece694252813766bb5924cd1d6928bf3ba
misterionqq <91603302+misterionqq@users.noreply.github.com>
2024-02-08 12:12:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,61 @@ struct structForTest {
     double time;
 };
 
+struct boolStructForTest {
+    longNumber first;
+    longNumber second;
+    short received;
+    short expected;
+    string operation;
+    bool flag;
+    double time;
+};
+
+boolStructForTest nowBoolTesting(const string& operation, const longNumber &first, const longNumber &second, short result) {
+    boolStructForTest test;
+
+    test.first = first;
+    test.second = second;
+
+    clock_t time1 = clock();
+
+    if (operation == ">") {
+        test.received = first > second;
+    } else {
+        if (operation == "<") {
+            test.received = first < second;
+        }
+        else {
+            if (operation == ">=") {
+                test.received = first >= second;
+            } else {
+                if (operation == "<=") {
+                    test.received = first <= second;
+                } else {
+                    if (operation == "==") {
+                        test.received = first == second;
+                    } else {
+                        if (operation == "!=") {
+                            test.received = first != second;
+                        } else {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    clock_t time2 = clock();
+
+    test.time = ((double) (time2 - time1)) * 1000 / CLOCKS_PER_SEC;
+    test.expected = result;
+    test.operation = operation;
+    test.flag = test.expected == test.received;
+
+    return test;
+}
+
 structForTest nowTesting(char operation, const longNumber &first, const longNumber &second, const longNumber &result) {
     structForTest test;
 
@@ -73,6 +128,30 @@ structForTest nowTesting(char operation, const longNumber &first, const longNumb
     test.flag = test.expected == test.received;
 
     return test;
+}
+
+void printBoolTestResults(const vector<boolStructForTest> &tests) {
+    size_t results = 0;
+
+    for (size_t i = 0; i < tests.size(); i++) {
+        results += tests[i].flag;
+
+        cout << "Test " << (i + 1) << ") ";
+
+        if (tests[i].flag)
+            cout << "OK (" << tests[i].time << " ms)";
+        else {
+            cout << "Failed: "
+                << tests[i].first << " " << tests[i].operation << " " << tests[i].second << " = "
+                << tests[i].expected
+                << " != " << tests[i].received;
+        }
+
+        cout << endl;
+    }
+
+    cout << "==================================" << endl;
+    cout << results << " / " << tests.size() << " (" << (100 * results / tests.size()) << "%)" << endl << endl;
 }
 
 void printTestResults(const vector<structForTest> &tests) {
@@ -193,10 +272,24 @@ void testOfRoots() {
     printTestResults(sqrts);
 }
 
+void testOfBools() {
+    std::vector<boolStructForTest> bools;
+    bools.push_back(nowBoolTesting(">", 0, 1, 0));
+    bools.push_back(nowBoolTesting("<", 1321, -21321, 0));
+    bools.push_back(nowBoolTesting(">=", longNumber("21321431541342.3413343241"), longNumber("21321431541342.3413343241"), 1));
+    bools.push_back(nowBoolTesting("<=", longNumber("21321431541342.9"), longNumber("21321431541342.8"), 0));
+    bools.push_back(nowBoolTesting("==", 314134, 314314312, 0));
+    bools.push_back(nowBoolTesting("!=", longNumber("13143214231.413241234"), longNumber("13143214231.413241234"), 0));
+
+    cout << "Test of bools\n" << endl;
+    printBoolTestResults(bools);
+}
+
 int main() {
     testOfAddition();
     testOfSubstaction();
     testOfMultiplication();
     testOfDivision();
+    testOfBools();
     testOfRoots();
 }
